a4a01096d593403833d56dd4df346de0f3bafcc1
mehedifiz <mehedi.shanto.contract@gmail.com>
2025-03-11 15:01:59 +0600

    FUTURE SECTION ADDED

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,7 @@ import OurProgram from "@/components/OurProgram";
 import Banner from "./Banner";
 import OnlineProgramSection from "@/components/OnlineProgramSection";
 import FeatureSection from "@/components/FeatureSection";
-import StayConnected from "@/components/StayConnected";
-import PricingSection from "@/components/PricingSection";
+ 
 
 const Home = () => {
    return (
@@ -11,7 +10,8 @@ const Home = () => {
          <Banner />
          <OurProgram/>
          <OnlineProgramSection/>
-       
+         <FeatureSection/>
+      
 
       </div>
    );
