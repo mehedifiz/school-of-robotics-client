32613f019c37796c5e77a9b23d0a6e3a051eac4a
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-03-13 20:48:37 +0600

    add login

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,16 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import ErrorPage from "../pages/error/ErrorPage";
 import Home from "../pages/home/Home";
+import Login from "../pages/login/login";
 
 const router = createBrowserRouter([
-   {
-      path: "/",
-      errorElement: <ErrorPage />,
-      element: <Root />,
-      children: [
-         { index: true, element: <Home /> },
-      ]
-   },
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+    ],
+  },
 ]);
 
 export default router;
